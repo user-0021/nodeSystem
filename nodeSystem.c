b8310f381ecf1b9493a5d7cae806fade9f001c94
user-0021 <nabeymaoden@gmail.com>
2025-01-11 17:41:33 +0900

    bug fix

--- nodeSystem.c
+++ nodeSystem.c
@@ -1482,7 +1482,7 @@ static void pipeNodeSetConst(){
 			res = -1;
 		}else{
 			//cpy data
-			if(shareMemoryOpen(&pipe_const->shm,0)  != 0){
+			if(shareMemoryOpen(&pipe_const->shm,0)  == 0){
 				shareMemoryLock(&pipe_const->shm);
 				((uint8_t*)pipe_const->shm.shmMap)[0]++;
 				memcpy(pipe_const->shm.shmMap+1,tmpBuffer,size*pipe_const->length);
